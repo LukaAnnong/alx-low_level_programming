49857d239d901a2f858b5238e4f7d13469035a6a
LukaAnnong <annongluka@yahoo.com>
2022-12-21 10:40:58 -0800

    task advance

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * cap_string - function that capitalizes all words of a string.
+ * cap_string - function that capitalizes all words of a string
  * @s: Pointer to Char
  * Return: char.
  */
@@ -10,27 +10,24 @@ char *cap_string(char *s)
 {
 	int i = 0;
 
-	while (*(s + i) != '\0')
+	while (s[i] != '\0')
 	{
-		if (i == 0 && (*(s + i) >= 97 && *(s + i) <= 122))
+		if (s[i] >= 97 && s[i] <= 122)
 		{
-			*(s + i) = *(s + i) - ' ';
-			i++;
-		}
-		if (*(s + i) == ' ' || *(s + i) == '\n' || *(s + i) == '\t'
-				|| *(s + i) == ',' || *(s + i) == ';' || *(s + i) == '!'
-				|| *(s + i) == '?' || *(s + i) == '"' || *(s + i) == '('
-				    || *(s + i) == ')' || *(s + i) == '{' || *(s + i) == '}'
-				    | *(s + i) == '.')
-		{
-			i++;
-			if (*(s + i) >= 97 && *(s + i) <= 122)
+			if (i == 0)
 			{
-				*(s + i) = *(s + i) - ' ';
+				s[i] -= 32;
+			}
+			if (s[i - 1] == 32 || s[i - 1] == 9 || s[i - 1] == 10 ||
+					s[i - 1] == 44 || s[i - 1] == 59 || s[i - 1] == 46 ||
+					s[i - 1] == 33 || s[i - 1] == 63 || s[i - 1] == 34 ||
+					s[i - 1] == 40 || s[i - 1] == 41 || s[i - 1] == 123 ||
+					s[i - 1] == 124)
+			{
+				s[i] -= 32;
 			}
 		}
-		else
-			i++;
+		i++;
 	}
 	return (s);
 }
