f34991996b9c6d4ac0410e91dbc661942e666b13
LukaAnnong <annongluka@yahoo.com>
2023-01-12 14:56:48 -0800

    update

--- 0x0C-more_malloc_free/main.h
+++ 0x0C-more_malloc_free/main.h
@@ -1,3 +1,5 @@
+#ifndef MAIN_H
+#define MAIN_H
 
 int _putchar(char c);
 void *malloc_checked(unsigned int b);
@@ -7,3 +9,8 @@ char *_memset(char *s, char b, unsigned int n);
 int *array_range(int min, int max);
 void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size);
 char *_memcpy(char *dest, char *src, unsigned int n);
+void errors(void);
+int _strlen(char *s);
+int is_digit(char *s);
+
+#endif
