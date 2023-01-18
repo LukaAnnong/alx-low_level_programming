d076385b6f9d1adc060eb035abadf6269121ffa9
LukaAnnong <annongluka@yahoo.com>
2023-01-18 10:33:31 -0800

    update

--- 0x0F-function_pointers/100-main_opcodes.c
+++ 0x0F-function_pointers/100-main_opcodes.c
@@ -1,44 +1,44 @@
-#include <stdlib.h>
+#include <string.h>
 #include <stdio.h>
+#include <stdlib.h>
 
 /**
- * print_opcodes - print the opcodes of this program
- * @a: address of the main function
- *  @n: number of bytes to print
+ * main - Entry point
  *
- *  Return: void
- */
-
-void print_opcodes(char *a, int n)
-{
-	int i;
-
-	for (i = 0; i < n; i++)
-	{
-		printf("%.2hhx", a[i]);
-		if (i < n - 1)
-			printf(" ");
-	}
-	printf("\n");
-}
-
-/**
- * main - prints the opcodes of its own main function
- * @argc: argument count
+ * Description: A program that prints the opcodes
+ * of its own main function
+ * Usage: ./main number_of_bytes
+ *
+ * @argc: argument counter
  * @argv: argument vector
  *
- *  Return: O
+ * Return: Always Successful
  */
 
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
-	int a;
+	int index, nbytes;
+	char *ptr = (char *) main;
 
 	if (argc != 2)
 	{
 		printf("Error\n");
 		exit(1);
 	}
-	print_opcodes((char *)&main, a);
+
+	nbytes = atoi(argv[1]);
+	if (nbytes < 0)
+	{
+		printf("Error\n");
+		exit(2);
+	}
+
+	for (index = 0; index < nbytes; index++)
+	{
+		printf("%02x", ptr[index] & 0xFF);
+		if (index != nbytes - 1)
+			printf(" ");
+	}
+	printf("\n");
 	return (0);
 }
